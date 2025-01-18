f5f0ab25f169d69eecb1890ab526ae487bcf6516
besma02 <besmaharzli@gmail.com>
2025-01-18 10:59:29 +0100

    tailwind config

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,8 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        lightGray: '#F8F8F8',
-        neonGreen: '#01FF19',
+        "bitchest-primary": "#38618C",
+        "bitchest-secondary": "#35A7FF",
+        "bitchest-white": "#FFFFFF",
+        "bitchest-black": "#434445",
+        "bitchest-alert": "#FF5964",
+        "bitchest-success": "#01FF19",
+         lightGray: '#F8F8F8',
+         neonGreen: '#01FF19',
       },
     },
   },
